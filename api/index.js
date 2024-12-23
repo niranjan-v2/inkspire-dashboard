da52d5413e0468ae1cdec23efbda0f60485f7b87
Niranjan Vasudevan <niranjanv233@gmail.com>
2024-12-23 09:09:10 +0800

    Create and commit API route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.DatabaseConnectionString)
-                .then(() => {
-                                console.log('Database is connected');
-                })
-                .catch((err) => {
-                                console.log(err);
-                });
+mongoose
+  .connect(process.env.DatabaseConnectionString)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-                console.log('Server is running on port 3000!!');
+  console.log("Server is running on port 3000!!");
 });
+
+app.use("/api/user", userRoute);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,25 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-        username: {
-                type: String,
-                required: true,
-                unique: true,
-        },
-        email: {
-                type: String,
-                required: true,
-                unique: trusted
-        },
-        password: {
-                type: String,
-                required: true
-        }}, 
-        {
-        timestamps:true
-        }       
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: trusted,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
