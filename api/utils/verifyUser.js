12e1e50a38df769dbcd322c06f3248aa834b13c5
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-06-18 12:46:53 +0530

    Resolve merge errors

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -5,11 +5,11 @@ import cookieParser from "cookie-parser";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(errorHandler(401, "Unauthorized"));
+    return next(errorHandler(401, "Unauthorized1"));
   }
   jwt.verify(token, process.env.JWT_SecretKey, (err, user) => {
     if (err) {
-      return next(errorHandler(401, "Unauthorized"));
+      return next(errorHandler(401, "Unauthorized2"));
     }
     req.user = user;
     next();
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,5 @@
 import { TextInput, Button, Alert, Modal } from "flowbite-react";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux";
 import {
   getDownloadURL,
@@ -22,6 +22,7 @@ import {
 import { useDispatch } from "react-redux";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { Link } from "react-router-dom";
+
 
 export default function DashProfile() {
   const { currentUser, error, loading } = useSelector((state) => state.user);
@@ -102,186 +103,187 @@ export default function DashProfile() {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (Object.keys(formData).length === 0) {
-      setUpdateUserError("Error, no changes made");
-      return;
-    }
-    if (imageFileUploading) {
-      return;
-    }
-    try {
-      dispatch(updateStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      if (!res.ok) {
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (Object.keys(formData).length === 0) {
+        return;
+      }
+      if (imageFileUploading) {
+        return;
+      }
+      try {
+        dispatch(updateStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          dispatch(updateFailure(data.message));
+        } else {
+          dispatch(updateSuccess(data));
+          setUpdateUserSuccess("Your profile has been updated");
+        }
+      } catch (error) {
         dispatch(updateFailure(data.message));
-      } else {
-        dispatch(updateSuccess(data));
-        setUpdateUserSuccess("Your profile has been updated");
       }
-    } catch (error) {
-      dispatch(updateFailure(data.message));
-    }
-  };
+    };
 
-  const handleSignout = async () => {
-    try {
-      const res = await fetch("/api/user/signout", {
-        method: "POST",
-      });
-      const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
-      } else {
-        dispatch(signoutSuccess());
+    const handleSignout = async () => {
+      try {
+        const res = await fetch("/api/user/signout", {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
       }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+    };
+  
 
-  return (
-    <div className="max-w-lg mx-auto p-3 w-full">
-      <h1 className="my-7 text-center font-semibold text-3xl"> Profile </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
-          ref={filePickerRef}
-          hidden
-        />
-        <div
-          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
-          onClick={() => filePickerRef.current.click()}
-        >
-          {imageFileUploadProgress && (
-            <CircularProgressbar
-              value={imageFileUploadProgress || 0}
-              text={`${imageFileUploadProgress}%`}
-              strokeWidth={5}
-              styles={{
-                root: {
-                  width: "100%",
-                  height: "100%",
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                },
-                path: {
-                  stroke: `rgba(62, 152, 199, ${
-                    imageFileUploadProgress / 100
-                  })`,
-                },
-              }}
-            />
-          )}
-          <img
-            src={imageFileUrl || currentUser.profilePicture}
-            alt="user"
-            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] 
+    return (
+      <div className="max-w-lg mx-auto p-3 w-full">
+        <h1 className="my-7 text-center font-semibold text-3xl"> Profile </h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={filePickerRef}
+            hidden
+          />
+          <div
+            className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+            onClick={() => filePickerRef.current.click()}
+          >
+            {imageFileUploadProgress && (
+              <CircularProgressbar
+                value={imageFileUploadProgress || 0}
+                text={`${imageFileUploadProgress}%`}
+                strokeWidth={5}
+                styles={{
+                  root: {
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                  },
+                  path: {
+                    stroke: `rgba(62, 152, 199, ${
+                      imageFileUploadProgress / 100
+                    })`,
+                  },
+                }}
+              />
+            )}
+            <img
+              src={imageFileUrl || currentUser.profilePicture}
+              alt="user"
+              className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] 
               ${
                 imageFileUploadProgress &&
                 imageFileUploadProgress < 100 &&
                 "opacity-60"
               }`}
-          />
-        </div>
-        {imageFileUploadError && (
-          <Alert color="failure">{imageFileUploadError}</Alert>
-        )}
-        <TextInput
-          type="text"
-          id="username"
-          placeholder="Username"
-          defaultValue={currentUser.username}
-          onChange={handleChange}
-        />
-        <TextInput
-          type="email"
-          id="email"
-          placeholder="Email"
-          defaultValue={currentUser.email}
-          onChange={handleChange}
-        />
-        <TextInput
-          type="password"
-          id="password"
-          placeholder="Password"
-          defaultValue="************"
-          onChange={handleChange}
-        />
-        <Button
-          type="submit"
-          color="blue"
-          disabled={loading || imageFileUploading}
-        >
-          Update
-        </Button>
-        {currentUser.isAdmin && (
-          <Link to={'/create-post'}>
-            <Button className="w-full" color="indigo" type="button">
-              Create a post
-            </Button>
-          </Link>
-        )}
-      </form>
-      <div className="text-red-500 flex justify-between mt-5">
-        <span
-          onClick={() => setShowModal(true)}
-          className="cursor-pointer font-medium"
-        >
-          Delete Account
-        </span>
-        <span onClick={handleSignout} className="cursor-pointer font-medium">
-          Sign Out
-        </span>
-      </div>
-      {updateUserSuccess && (
-        <Alert color="success" className="mt-5">
-          {updateUserSuccess}
-        </Alert>
-      )}
-
-      {updateUserError && (
-        <Alert color="failure" className="mt-5">
-          {updateUserError}
-        </Alert>
-      )}
-
-      {error && (
-        <Alert color="failure" className="mt-5">
-          {error}
-        </Alert>
-      )}
-
-      <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete your account
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeleteUser}>
-                Yes
-              </Button>
-              <Button color="gray" onClick={() => setShowModal(false)}>
-                No
-              </Button>
-            </div>
+            />
           </div>
-        </Modal.Body>
-      </Modal>
-    </div>
-  );
-}
+          {imageFileUploadError && (
+            <Alert color="failure">{imageFileUploadError}</Alert>
+          )}
+          <TextInput
+            type="text"
+            id="username"
+            placeholder="Username"
+            defaultValue={currentUser.username}
+            onChange={handleChange}
+          />
+          <TextInput
+            type="email"
+            id="email"
+            placeholder="Email"
+            defaultValue={currentUser.email}
+            onChange={handleChange}
+          />
+          <TextInput
+            type="password"
+            id="password"
+            placeholder="Password"
+            defaultValue="************"
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            color="blue"
+            disabled={loading || imageFileUploading}
+          >
+            Update
+          </Button>
+          {currentUser.isAdmin && (
+            <Link to={"/create-post"}>
+              <Button className="w-full" color="indigo" type="button">
+                Create a post
+              </Button>
+            </Link>
+          )}
+        </form>
+        <div className="text-red-500 flex justify-between mt-5">
+          <span
+            onClick={() => setShowModal(true)}
+            className="cursor-pointer font-medium"
+          >
+            Delete Account
+          </span>
+          <span onClick={handleSignout} className="cursor-pointer font-medium">
+            Sign Out
+          </span>
+        </div>
+        {updateUserSuccess && (
+          <Alert color="success" className="mt-5">
+            {updateUserSuccess}
+          </Alert>
+        )}
+
+        {updateUserError && (
+          <Alert color="failure" className="mt-5">
+            {updateUserError}
+          </Alert>
+        )}
+
+        {error && (
+          <Alert color="failure" className="mt-5">
+            {error}
+          </Alert>
+        )}
+
+        <Modal show={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+              <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                Are you sure you want to delete your account
+              </h3>
+              <div className="flex justify-center gap-4">
+                <Button color="failure" onClick={handleDeleteUser}>
+                  Yes
+                </Button>
+                <Button color="gray" onClick={() => setShowModal(false)}>
+                  No
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+};
+
