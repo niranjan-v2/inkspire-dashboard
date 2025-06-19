3cfbff296708b4022541b5daac3b103344cd07ba
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-06-19 22:23:05 +0530

    fix: remove unnecessary object wrapper from create post response

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -14,7 +14,7 @@ export const create = async(req, res, next) => {
     });
     try {
         const savedPost = await newPost.save();
-        res.status(201).json({savedPost});
+        res.status(201).json(savedPost);
     } catch(error) {
         next(error)
     }
