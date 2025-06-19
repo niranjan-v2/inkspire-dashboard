ac4832609caa252224a3779d51cf43ce225f878f
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-06-19 22:46:46 +0530

    Add posts section to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,23 +3,28 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import DashProfile from "../components/DashProfile";
 import DashSidebar from "../components/DashSidebar";
+import DashPosts from "../components/DashPosts";
 
 export default function Dashboard() {
-  const location  = useLocation();
-  const [tab, setTab] = useState('');
+  const location = useLocation();
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab');
-    if(tabFromUrl) { 
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-  return <div className="min-h-screen flex flex-col md:flex-row">
-    <div className="md:w-56"> 
-      {/* Sidebar */}
-      <DashSidebar />
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* Profile */}
+      {tab === "profile" && <DashProfile />}
+      {/* Posts */}
+      {tab === "posts" && <DashPosts />}
     </div>
-    {/* Profile */}
-    {tab === 'profile' && <DashProfile />}
-  </div>;
+  );
 }
