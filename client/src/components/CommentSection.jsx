a0c96d9545b173ff098d303af5065d5455c8c61e
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-06-27 13:27:53 +0530

    Handle comment submit from the front-end

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,4 +1,4 @@
-import { Button, Textarea } from "flowbite-react";
+import { Alert, Button, Textarea } from "flowbite-react";
 import React from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -6,35 +6,85 @@ import { Link } from "react-router-dom";
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
-  const [comment, setComment] = useState('');
-  const handleSubmit = async(e) => {
-    try {
-
-    } catch(error) {
-
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) {
+      return;
     }
-  }
+    try {
+      const res = await fetch(`/api/comment/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          postId,
+          userId: currentUser._id,
+          content: comment,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
         <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
           <p>Signed in as: </p>
-          <img className="h-5 w-5 object-cover rounded-full" src={currentUser.profilePicture} alt="" />
-          <Link className="text-xs text-indigo-950 dark:text-indigo-300 hover:underline" to={"/dashboard?tab=profile"}>@{currentUser.username}</Link>
+          <img
+            className="h-5 w-5 object-cover rounded-full"
+            src={currentUser.profilePicture}
+            alt=""
+          />
+          <Link
+            className="text-xs text-indigo-950 dark:text-indigo-300 hover:underline"
+            to={"/dashboard?tab=profile"}
+          >
+            @{currentUser.username}
+          </Link>
         </div>
       ) : (
         <div className="text-sm text-gray-800 dark:text-gray-300 flex gap-1">
-            You must me signed in to comment. 
-            <Link className="text-blue-700 hover:underline" to={"/signin"}>Sign in</Link>
+          You must me signed in to comment.
+          <Link className="text-blue-700 hover:underline" to={"/signin"}>
+            Sign in
+          </Link>
         </div>
       )}
       {currentUser && (
-        <form onSubmit={handleSubmit} className="border border-teal-500 dark:border-gray-500 rounded-md p-3">
-            <Textarea className="focus:border-emerald-400" placeholder="Add a comment..." rows='3' maxLength='200' onChange={(e) => setComment(e.target.value)} value={comment} />
-            <div className="flex justify-between items-center mt-5 ">
-                <p className="text-gray-500 text-sm">{200 - comment.length} characters remaining</p>
-                <Button outline gradientDuoTone="purpleToBlue" type="submit">Submit</Button>
-            </div>
+        <form
+          onSubmit={handleSubmit}
+          className="border border-teal-500 dark:border-gray-500 rounded-md p-3"
+        >
+          <Textarea
+            className="focus:border-emerald-400"
+            placeholder="Add a comment..."
+            rows="3"
+            maxLength="200"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className="flex justify-between items-center mt-5 ">
+            <p className="text-gray-500 text-sm">
+              {200 - comment.length} characters remaining
+            </p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
+          </div>
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
         </form>
       )}
     </div>
