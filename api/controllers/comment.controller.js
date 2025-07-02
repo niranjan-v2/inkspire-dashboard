35fb75c70d5e653c9d46f0ae58da8807c7a1e30b
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-07-02 09:38:11 +0530

    Create edit-comment API route

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,53 +1,75 @@
 import Comment from "../models/comment.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createComment = async(req, res, next) => {
-    try {
-        const {postId, userId, content} = req.body;
-        if(userId !== req.user.id) {
-            return next(errorHandler(403, 'You are not allowed to add this comment'));
-        }
-        
-        const newComment = new Comment({postId, userId, content});
-        await newComment.save();
-        res.status(200).json(newComment);
-
-    } catch(error) {
-        next(error);
+export const createComment = async (req, res, next) => {
+  try {
+    const { postId, userId, content } = req.body;
+    if (userId !== req.user.id) {
+      return next(errorHandler(403, "You are not allowed to add this comment"));
     }
-}
 
-export const getPostComments = async(req, res, next) => {
-    try {
-        const comments = await Comment.find({postId: req.params.postId}).sort({
-            createdAt: -1,
-        });
-        res.status(200).json(comments);
-    } catch(error) {
-        next(error);
-    }
-}
+    const newComment = new Comment({ postId, userId, content });
+    await newComment.save();
+    res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const likeComment = async(req, res, next) => {
-    try {
-        const comment = await Comment.findById(req.params.commentId);
-        if(!comment) {
-            return next(errorHandler(404, 'Comment not found'));
-        }
-        const userIndex = comment.likes.indexOf(req.user.id);
-        if(userIndex === -1) {
-            console.log('here1 - new like');
-            comment.numberOfLikes += 1;
-            comment.likes.push(req.user.id);
-        }
-        else {
-            console.log('here2 - unliked');
-            comment.numberOfLikes -= 1;
-            comment.likes.splice(userIndex, 1);
-        }
-        await comment.save();
-        res.status(200).json(comment);
-    } catch(error) {
-        next(error);
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const likeComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
     }
-}
+    const userIndex = comment.likes.indexOf(req.user.id);
+    if (userIndex === -1) {
+      console.log("here1 - new like");
+      comment.numberOfLikes += 1;
+      comment.likes.push(req.user.id);
+    } else {
+      console.log("here2 - unliked");
+      comment.numberOfLikes -= 1;
+      comment.likes.splice(userIndex, 1);
+    }
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not allowed to edit this comment")
+      );
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
+    res.status(200).json(editComment);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
+import { createComment, getPostComments, likeComment, editComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getcomments/:postId', getPostComments);
 router.put('/like/:commentId', verifyToken, likeComment);
+router.put('/edit/:commentId', verifyToken, editComment);
 
 export default router;
 
