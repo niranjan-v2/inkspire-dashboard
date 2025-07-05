e78e50323e004e24fa3d8be4c6e58324d0c66221
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-07-05 13:08:20 +0530

    Add delete comment API end-point

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment, getPostComments, likeComment, editComment } from "../controllers/comment.controller.js";
+import { createComment, getPostComments, likeComment, editComment, deleteComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.post('/create', verifyToken, createComment);
 router.get('/getcomments/:postId', getPostComments);
 router.put('/like/:commentId', verifyToken, likeComment);
 router.put('/edit/:commentId', verifyToken, editComment);
+router.put('/delete/:commentId', verifyToken, deleteComment);
 
 export default router;
 
