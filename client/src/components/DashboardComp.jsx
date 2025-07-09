ad52abda0e4d4617db36836288c68ba620c498b9
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-07-09 10:47:55 +0530

    Conditionally executed fetch calls only for admin users in dashboard component

---
+++ client/src/components/DashboardComp.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+export default function DashboardComp() {
+  const [users, setUsers] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [lastMonthUsers, setLastMonthUsers] = useState(0);
+  const [lastMonthPosts, setLasMonthPosts] = useState(0);
+  const [lastMonthComments, setLastMonthComments] = useState(0);
+  const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`/api/user/getusers?limit=5`);
+        const data = await res.json();
+        if (res.ok) {
+          setUsers(data.users);
+          setTotalUsers(data.totalUsers);
+          setLastMonthUsers(data.lastMonthUsers);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?limit=5`);
+        const data = await res.json();
+        if (res.ok) {
+          setPosts(data.posts);
+          setTotalPosts(data.totalPosts);
+          setLastMonthPosts(data.lastMonthPosts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    const fetchComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getusercomments?limit=5`);
+        const data = await res.json();
+        if (res.ok) {
+          setComments(data.comments);
+          setTotalComments(data.totalComments);
+          setLastMonthComments(data.lastMonthComments);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchUsers();
+      fetchPosts();
+      fetchComments();
+    }
+  }, [currentUser]);
+  return <div>DashboardComp</div>;
+}
+
