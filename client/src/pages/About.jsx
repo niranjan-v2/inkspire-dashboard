9803a9539ccb719806e0808de51733880f743e6e
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-07-14 00:24:50 +0530

    Complete about page and embed resume

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
 export default function About() {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.mjs",
+    import.meta.url
+  ).toString();
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
   return (
-    <div>About</div>
-  )
+    <div className="flex flex-col items-center py-8">
+        <Document file="/Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page scale={1.5} pageNumber={pageNumber} renderTextLayer={false} renderAnnotationLayer={false} />
+        </Document>
+    </div>
+  );
 }
